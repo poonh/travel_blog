585c219056bfbe112b3f4fc2ecafe4e2c3fcacec
poonh <95615486+poonh@users.noreply.github.com>
2024-11-28 18:04:02 +0800

    Add files via upload

--- search_script.js
+++ search_script.js
@@ -34,18 +34,15 @@ function searchArticles() {
     });
 
     // Display results
-    if (filteredArticles.length > 0) {
-      filteredArticles.forEach(article => {
-        const resultItem = document.createElement("div");
-        resultItem.innerHTML = `
-          <a href="${article.url}" target="_blank"><h3>${article.title}</h3></a>
-          <p>${article.description}</p>
-        `;
-        resultsContainer.appendChild(resultItem);
-      });
-    } else {
-      resultsContainer.innerHTML = "<p>没有结果</p>";
-    }
+      if (filteredArticles.length > 0) {
+        // Store search results in sessionStorage (this is temporary data for the session)
+        sessionStorage.setItem('searchResults', JSON.stringify(filteredArticles));
+
+        // Open a new tab with the search results
+        window.open('search_results.html', '_blank');
+      } else {
+        alert('没有结果');
+      }
   }
 }
 
